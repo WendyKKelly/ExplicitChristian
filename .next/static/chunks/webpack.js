4481e20d8152197d49ce852cab8cf702204369de
Wendy Kelly <wendykelly@Wendys-iMac.local>
2021-05-23 18:32:55 -0700

    took out stock stuff

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "69a56646979c9695ec6e";
+/******/ 	var hotCurrentHash = "fb8dfc012da7da42a514";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
