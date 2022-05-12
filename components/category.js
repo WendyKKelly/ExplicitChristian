6a425d480d4189de96a3b2229f4332c274b59e8a
Wendy Kelly <wendykelly@Wendys-iMac.local>
2022-05-12 10:16:20 -0700

    save all

--- components/category.js
+++ components/category.js
@@ -3,20 +3,16 @@ import CategoryPreview from '../components/category-preview'
 export default function Category({ posts }) {
     return (
     <section>
-      <h2 className="mb-8 text-6xl md:text-7xl font-bold tracking-tighter leading-tight">
-        Categories
-      </h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32">
+     
+      <div className="grid grid-cols-1 md:grid-cols-1 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32">
         {posts.map((post) => (
           <CategoryPreview
             key={post.slug}
-            title={post.title}
+            
             topic={post.topic}
-            coverImage={post.coverImage}
-            date={post.date}
-            author={post.author}
+           
             slug={post.slug}
-            excerpt={post.excerpt}
+           
           />
         ))}
       </div>
@@ -24,13 +20,3 @@ export default function Category({ posts }) {
   )
 }
 //
-<div>
-          <h3 className="mb-4 text-4xl lg:text-6xl leading-tight">
-            <Link as={`/posts/${slug}`} href="/posts/[slug]">
-              <a className="hover:bg-yellow"> 
-          <Category
-          />
-        </a>
-            </Link>
-          </h3>
-          </div>
