448552f3164ffa4d3db06265d324bd9b11d16256
Wendy Kelly <wendykelly@Wendys-iMac.local>
2022-05-22 11:01:39 -0700

    change to topic

--- pages/topic/[topic].js
+++ pages/topic/[topic].js
@@ -1,6 +1,44 @@
 import Layout from '../../components/layout';
 
-export default function Post() {
+export default function Topic() {
   return <Layout>...</Layout>;
+}
+
+export async function getStaticProps({ params }) {
+  const post = getPostBySlug(params.slug, [
+    'title',
+    'topic',
+    'date',
+    'slug',
+    'author',
+    'content',
+    'ogImage',
+    'coverImage',
+  ])
+  const content = await markdownToHtml(post.content || '')
+
+  return {
+    props: {
+      post: {
+        ...post,
+        content,
+      },
+    },
+  }
+}
+
+export async function getStaticPaths() {
+  const topic = getAllPosts(['topic'])
+
+  return {
+    paths: topic.map((topic) => {
+      return {
+        params: {
+          topic: topic.topic,
+        },
+      }
+    }),
+    fallback: false,
+  }
 }
 
