5f07101325748fad8eeb731f2a7fc7e19aac8d60
Wendy Kelly <wendykelly@Wendys-iMac.local>
2022-05-30 14:25:35 -0700

    attempt to add by topic

---
+++ components/articles.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import type { PostMeta } from "../lib/api";
+
+
+export default function Articles({ posts }: { posts: PostMeta[] }) {
+  return (
+    <ul >
+      {posts.map((post) => (
+        <li key={post.slug}>
+          <div >
+            <Link href={`/posts/${post.slug}`}>{post.title}</Link>
+          </div>
+          <p>{post.excerpt}</p>
+          <p >
+            {post.topic.map((topic) => (
+              <Link key={topic} href={`/tags/${topic}`}>
+                {topic}
+              </Link>
+            ))}
+          </p>
+        </li>
+      ))}
+    </ul>
+  );
+}
---
+++ pages/topic/[slug].tsx
@@ -0,0 +1,50 @@
+
+
+import type { GetStaticProps, GetStaticPaths } from "next";
+import { getAllPosts, PostMeta } from '../../lib/api'
+import Head from "next/head";
+import Articles from '../../components/articles';
+
+export default function TopicPage({
+  slug,
+  posts,
+}: {
+  slug: string;
+  posts: PostMeta[];
+}) {
+  return (
+    <>
+      <Head>
+        <title>Tag: {slug}</title>
+      </Head>
+      <h1>Tag: {slug}</h1>
+     
+      <Articles posts={posts} />
+      
+    </>
+  );
+}
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { slug } = params as { slug: string };
+  const posts = getAllPosts().filter((post) => post.meta.topic.includes(slug));
+
+  return {
+    props: {
+      slug,
+      posts: posts.map((post) => post.meta),
+    },
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const posts = getAllPosts();
+  const topic = new Set(posts.map((post) => post.meta.topic).flat());
+  const paths = Array.from(topic).map((topic) => ({ params: { slug: topic } }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
