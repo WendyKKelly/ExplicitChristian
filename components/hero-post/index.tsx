f44a1fce698adf21ccc1d5808622b362dc0d244b
Wendy Kelly <wendykelly@Wendys-iMac.local>
2022-06-02 12:28:29 -0700

    css on hero-post

--- components/hero-post/index.tsx
+++ components/hero-post/index.tsx
@@ -5,11 +5,12 @@ import Link from 'next/link'
 export default function HeroPost({ posts }: { posts: PostMeta[] }) {
 
   return (
+    <section>
     <ul >
-      {posts.map((post) => (
-        <li key={post.slug}>
+ {posts.map((post) => (
+   <li key={post.slug}>
           <div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28 hover:bg-yellow">
-        
+       
           <div >
           <PostHeader3
                
@@ -19,7 +20,7 @@ export default function HeroPost({ posts }: { posts: PostMeta[] }) {
                 
               />
         </div>
-        </div>
+        
         <div>
           <ul><li><h3 className="mb-4 text-4xl lg:text-6xl leading-tight"><ul>
 
@@ -36,23 +37,25 @@ export default function HeroPost({ posts }: { posts: PostMeta[] }) {
           </h3></li>
           </ul>
           </div>
+          </div>
         <div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
         <div>
           <h3 className="mb-4 text-4xl lg:text-6xl leading-tight">
-        
-          
-            
-              <Link key={post.topic} href={`/topic/${post.topic}`}>
-                {post.topic}
-              </Link>
+          <Link as={`/posts/${post.slug}`} href="/posts/[slug]">
+              <a className="hover:bg-yellow">{post.title}</a>
+            </Link>
             
           </h3>
+          <div className="mb-4 md:mb-0 text-lg">
           </div>
+          </div>
+          <div>
           <p className="text-lg leading-relaxed mb-4">{post.excerpt}</p>
           </div>
-        </li>
-      ))}
-    </ul>
-    
+          </div>
+       </li>
+       ))}
+     </ul>
+   </section> 
   );
 }
