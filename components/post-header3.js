1e82b93738c781b63940f078e06ebdcaa9a6a4eb
Wendy Kelly <wendykelly@Wendys-iMac.local>
2022-06-01 19:06:51 -0700

    took out title

--- components/post-header3.js
+++ components/post-header3.js
@@ -1,7 +1,7 @@
 
 
 import CoverImage from '../components/cover-image'
-import PostTitle from '../components/post-title'
+
 
 
 export default function PostHeader3({ coverImage, }) {
@@ -13,7 +13,7 @@ export default function PostHeader3({ coverImage, }) {
         
       </div>
       <div className="mb-8 md:mb-16 sm:mx-0">
-        <CoverImage title={title} src={coverImage} height={620} width={1240} />
+        <CoverImage  src={coverImage} height={620} width={1240} />
       </div>
       <div className="max-w-2xl mx-auto">
         <div className="block md:hidden mb-6">
