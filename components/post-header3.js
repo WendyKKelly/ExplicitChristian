c772bec3dc0549004239304164800137a252df2e
Wendy Kelly <wendykelly@Wendys-iMac.local>
2022-06-03 14:31:05 -0700

    clean up post header 3

--- components/post-header3.js
+++ components/post-header3.js
@@ -9,18 +9,11 @@ export default function PostHeader3({ coverImage, }) {
     <>
       
      
-      <div className="hidden md:block md:mb-12">
-        
-      </div>
+      
       <div className="mb-8 md:mb-16 sm:mx-0">
         <CoverImage  src={coverImage} height={620} width={1240} />
       </div>
-      <div className="max-w-2xl mx-auto">
-        <div className="block md:hidden mb-6">
-         
-        </div>
-     
-      </div>
+    
     </>
   )
 }
